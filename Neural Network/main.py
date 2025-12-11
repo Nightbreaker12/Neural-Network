38ecffc391496a2f7878448fc0fcca584c2aebfd
Xatheod <101279342+Nightbreaker12@users.noreply.github.com>
2025-12-11 14:17:55 +0100

    Added Loss/page 132

--- Neural Network/main.py
+++ Neural Network/main.py
@@ -26,7 +26,34 @@ class Activation_Softmax():
 
         self.output = probabilities
 
+class Loss:
+    def calculate(self,output,y):
+        sample_losses = self.forward(output,y)
 
+        data_loss = np.mean(sample_losses)
+
+        return data_loss
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self,y_pred,y_true):
+
+        samples = len(y_pred)
+
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped [ 
+                range(samples), y_true
+            ]
+
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+                y_pred_clipped * y_true, axis=1
+            )
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+    
 X, y = spiral_data(samples=100, classes=3)
 
 dense1 = Layer_Dense(2,3)
@@ -37,6 +64,8 @@ dense2 = Layer_Dense(3,3)
 
 actication2 = Activation_Softmax()
 
+loss_function = Loss_CategoricalCrossentropy()
+
 dense1.forward(X)
 
 actication1.forward(dense1.output)
@@ -46,3 +75,7 @@ dense2.forward(actication1.output)
 actication2.forward(dense2.output)
 
 print(actication2.output[:5])
+
+loss = loss_function.calculate(actication2.output, y)
+
+print('loss:', loss)
